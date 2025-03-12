34e78274104932d2161ae7f718e453d4b5a7f0cc
lee <2733827845@qq.com>
2025-03-12 13:00:56 +0800

    nio seletor 来进行channel管理

--- src/test/java/cn/itcast/netty/c4/SelectorServer.java
+++ src/test/java/cn/itcast/netty/c4/SelectorServer.java
@@ -5,11 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import static cn.itcast.netty.c1.ByteBufferUtil.debugRead;
@@ -27,37 +25,54 @@ public class SelectorServer {
          * read -可读事件
          * write -可写事件
          */
-
-
-
-
-        ByteBuffer buffer=ByteBuffer.allocate(16);
         ServerSocketChannel ssc=ServerSocketChannel.open();
         ssc.configureBlocking(false);
 
         //2.建立Selector和channel的联系（注册）
-        //SelectionKey 就是将来事件发生后，通过它可以知道事件和哪个channel的事件
+        //SelectionKey 就是将来事件发生后，通过它可以知道事件和哪个channel的事件 这里初始值为0 表示不关注任何事件
         SelectionKey sscKey = ssc.register(selector, 0, null);
-
-
+        //key 只关注accept 事件 这里把0改为16 可以看见这里这个 final int OP_ACCEPT = 16 为16 表示关注accept事件
+        sscKey.interestOps(SelectionKey.OP_ACCEPT);
+        log.debug("register key:{}",sscKey);
         ssc.bind(new InetSocketAddress(8080));
-        List<SocketChannel> channels=new ArrayList<>();
         while (true){
-            SocketChannel sc = ssc.accept();
-            if (sc!=null){
-                log.debug("connected....{}",sc);
-                sc.configureBlocking(false);
-                channels.add(sc);
-            }
-            for (SocketChannel channel : channels) {
-                int read = channel.read(buffer);
-                if (read>0){
+            //3.select 方法   没有事件发生，select会阻塞 但是有事件发生 会让线程不阻塞
+            //select 在事件未处理时候 它不会阻塞     事件发生后要么处理要么取消 不可以置之不理
+            selector.select();
+            //4.处理事件  拿去所有的可用事件的  SelectionKeys内部包含了所有发生的事件
+            //ps 你想在集合遍历时候删除得用迭代器遍历
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator(); //accept ,read
+            while (iterator.hasNext()){
+                SelectionKey key = iterator.next();
+                log.debug("key:{}",key);
+                //区分事件类型
+                if (key.isAcceptable()) {
+                    //如果是accept事件
+                    //处理事件
+                    ServerSocketChannel channel = (ServerSocketChannel)key.channel();
+                    SocketChannel sc = channel.accept();
+                    log.debug("{}",sc);
+                    //把SocketChannel也设置为不阻塞
+                    sc.configureBlocking(false);
+                    //同时把这个SocketChannel 也放进selector进行管理 SelectionKe == 返回对应的key
+                    SelectionKey scKey = sc.register(selector, 0, null);
+                    log.debug("key:{}",scKey);
+                    //关注读
+                    scKey.interestOps(SelectionKey.OP_READ);
+                } else if (key.isReadable()) {
+                    //如果是read事件
+                    SocketChannel channel = (SocketChannel)key.channel();//拿到触发事件的channel
+                    ByteBuffer buffer=ByteBuffer.allocate(16);
+                    channel.read(buffer);
                     buffer.flip();
                     debugRead(buffer);
-                    buffer.clear();
-                    log.debug("after read...{}",channel);
+
                 }
+
+                //取消事件
+//                key.cancel();
             }
+
         }
 
 
