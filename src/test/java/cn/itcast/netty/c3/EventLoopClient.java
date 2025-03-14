f4b6fd7fdb60ae8f009b3460263446dba6a81b69
李志 <2733827845@qq.com>
2025-03-14 11:01:58 +0800

    netty channelFuture 两种不同的处理  同步和异步

--- src/test/java/cn/itcast/netty/c3/EventLoopClient.java
+++ src/test/java/cn/itcast/netty/c3/EventLoopClient.java
@@ -2,32 +2,48 @@ package cn.itcast.netty.c3;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class EventLoopClient {
     public static void main(String[] args) throws InterruptedException {
-
-        //1.创建启动器类
-        Channel channel = new Bootstrap()
-                //2.添加 EventLoop
+        //2.带Future ,Promise 的类型都是和异步方法配套使用，用来处理结果
+        ChannelFuture channelFuture = new Bootstrap()
                 .group(new NioEventLoopGroup())
-                //3.选择客户端channel事件
                 .channel(NioSocketChannel.class)
-                //4.添加处理器
                 .handler(new ChannelInitializer<NioSocketChannel>() {
                     @Override//在连接建立后被调用
                     protected void initChannel(NioSocketChannel ch) throws Exception {
                         ch.pipeline().addLast(new StringEncoder());
                     }
                 })
-                //5.连接服务器
-                .connect("localhost", 8080)
-                .sync() //阻塞方法 直到连接建立完成后才可以往下运行
-                .channel();
-        System.out.println(channel);
-        System.out.println("");
+                //1.连接服务器
+                // 异步非阻塞 main发起调用 真正执行连接 connect 是NioEventLoopGroup 中的一个线程
+                .connect("localhost", 8080);  //1s才能把这个连接建立好
+        //2.1使用sync 方法同步处理结果
+        /*channelFuture.sync(); //阻塞方法 直到nio连接建立完成后才可以往下运行
+        //无阻塞的向下获取channel
+        Channel channel = channelFuture.channel();
+        log.debug("{}",channel);
+        channel .writeAndFlush("hello world");*/
+
+        //2.2使用 addListener（回调对象） 方法异步处理结果
+        channelFuture.addListener(new ChannelFutureListener() {
+            @Override
+            //在nio线程连接建立好了之后，会调用 operationComplete 方法
+            public void operationComplete(ChannelFuture future) throws Exception {
+                Channel channel = future.channel();
+                log.debug("线程是：{}",channel);
+                channel.writeAndFlush("你好");
+            }
+        });
+
+
     }
 }
