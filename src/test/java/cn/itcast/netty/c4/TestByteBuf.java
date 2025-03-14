7b93bfc8bac6aede98fb1f82f5fbb21ecdfe919d
李志 <2733827845@qq.com>
2025-03-14 17:31:44 +0800

    netty ByteBuf

---
+++ src/test/java/cn/itcast/netty/c4/TestByteBuf.java
@@ -0,0 +1,40 @@
+package cn.itcast.netty.c4;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+
+import static io.netty.buffer.ByteBufUtil.appendPrettyHexDump;
+import static io.netty.util.internal.StringUtil.NEWLINE;
+
+public class TestByteBuf {
+    public static void main(String[] args) {
+        /*//使用堆内存 heap 快初始化 慢读写
+        ByteBuf buf = ByteBufAllocator.DEFAULT.heapBuffer();
+        //使用直接内存 heap 快读写 慢初始化 不受CG影响 注意主动释放
+        ByteBuf buf = ByteBufAllocator.DEFAULT.directBuffer();*/
+        //使用默认内存==直接内存
+        ByteBuf buf = ByteBufAllocator.DEFAULT.buffer();
+        System.out.println(buf.getClass());
+        log(buf);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 32; i++) {
+            sb.append("a");
+        }
+
+        buf.writeBytes(sb.toString().getBytes());
+        log(buf);
+    }
+
+    private static void log(ByteBuf buffer) {
+        int length = buffer.readableBytes();
+        int rows = length / 16 + (length % 15 == 0 ? 0 : 1) + 4;
+        StringBuilder buf = new StringBuilder(rows * 80 * 2)
+                .append("read index:").append(buffer.readerIndex())
+                .append(" write index:").append(buffer.writerIndex())
+                .append(" capacity:").append(buffer.capacity())
+                .append(NEWLINE);
+        appendPrettyHexDump(buf, buffer);
+        System.out.println(buf.toString());
+    }
+}
